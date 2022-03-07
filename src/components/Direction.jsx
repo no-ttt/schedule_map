621aa7a186b526b8a35af6bcc6e61febdbb5eb67
no-ttt <dear890329@gmail.com>
2022-03-07 17:05:21 +0800

    點到點的路線

---
+++ src/components/Direction.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { GoogleMap, LoadScript, DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
+
+const containerStyle = {
+  height: '100vh'
+};
+
+const center = {
+  lat: 23.968249192607157,
+  lng: 120.95896959995788
+};
+
+export default class Direction extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      response: null,
+      travelMode: 'DRIVING',
+      origin: '545南投縣埔里鎮中山路三段676號545',
+      destination: '545南投縣埔里鎮中山路三段245號',
+    }
+    this.directionsCallback = this.directionsCallback.bind(this)
+  }
+
+  directionsCallback (response) {
+    console.log(response)
+
+    if (response !== null) {
+      if (response.status === 'OK') {
+        this.setState(
+          () => ({
+            response
+          })
+        )
+      } else {
+        console.log('response: ', response)
+      }
+    }
+  }
+
+  render() {
+    return (
+      <LoadScript
+        googleMapsApiKey="AIzaSyC7-6v1eCkrtOESIW9B5UMms2oUgxdP7wA"
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={18}
+        >
+          <DirectionsService
+          options={{
+            destination: this.state.destination,
+            origin: this.state.origin,
+            travelMode: this.state.travelMode
+          }}
+          callback={this.directionsCallback}
+        />
+        {
+          this.state.response !== null && (
+            <DirectionsRenderer
+              options={{ 
+                directions: this.state.response
+              }}
+            />
+          )
+        }
+        </GoogleMap>
+      </LoadScript>
+    )
+  }
+}
